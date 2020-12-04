00a17bf1c896294b66631d52224cde986722d9cf
Ketchup288 <arsenko525@gmail.com>
2020-12-04 17:27:56 +0200

    New file added

---
+++ lab_4_2.cpp
@@ -0,0 +1,43 @@
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    double x, y, A, B;
+    double xp, xk, dx; // Аргументи, для вводу з клавіатури
+
+    cout << "Please, input xp: "; cin >> xp; // X_початкова
+    cout << "Please, input xk: "; cin >> xk; // X_кінцева
+    cout << "Please, input dx: "; cin >> dx; // Крок функції
+
+    cout << fixed;
+
+    cout << "---------------------------" << endl;
+    cout << "|" << setw(5) << "x" << " |" << setw(7) << "y" << " |" << endl;
+    cout << "---------------------------" << endl;
+
+    x = xp;
+    while(x<=xk){
+        A = 2 * pow(abs(x),3);
+        if (x<=-0.1){
+            B = 5 * cos(18*x);
+        }
+        else{
+            if (x>-0.1 && x < 1.2){
+                    B = atan((x+2)/5);
+        }
+            else{
+                    B = cos(x+18)/sin(x+18);
+        }
+        }
+        y = A - B;
+        cout << "|" << setw(7) << setprecision(2) << x
+        << " |" << setw(10) << setprecision(3) << y
+        << " |" << endl;
+        x += dx;
+    }
+    cout << "---------------------------" << endl;
+
+}
